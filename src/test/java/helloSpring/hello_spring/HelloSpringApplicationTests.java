a6d6fdd3b6420bbb08c71584cee97c23675708ed
= <yeokyeong1004@gmail.com>
2025-03-04 22:06:06 +0900

    set project

---
+++ src/test/java/helloSpring/hello_spring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package helloSpring.hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
