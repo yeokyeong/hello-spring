e22627534fb0e60730a9930b6f2328d2d2f2a668
= <yeokyeong1004@gmail.com>
2025-03-05 11:28:02 +0900

    implemented member service

---
+++ src/main/java/helloSpring/hello_spring/service/MemberService.java
@@ -0,0 +1,41 @@
+package helloSpring.hello_spring.service;
+
+import helloSpring.hello_spring.domain.Member;
+import helloSpring.hello_spring.repository.MemberRepository;
+import helloSpring.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 중복회원 X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체회원조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
