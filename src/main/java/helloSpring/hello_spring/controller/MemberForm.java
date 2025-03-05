1b5c2d1d727b228a5a6282c65c925d06296c54e7
= <yeokyeong1004@gmail.com>
2025-03-05 15:15:04 +0900

    added MemberForm

---
+++ src/main/java/helloSpring/hello_spring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package helloSpring.hello_spring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
